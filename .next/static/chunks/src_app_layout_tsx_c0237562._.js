60b55134c1cc0906633ee42a429091fa364c2b0a
ramcodeverse <ramcodeverse@gmail.com>
2025-07-21 20:25:33 +0530

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1753109730807

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__8ebb6d4b._.css",
+  "static/chunks/node_modules_d4065aff._.js",
+  "static/chunks/src_components_ui_sonner_tsx_346cfefb._.js"
+],
+    source: "dynamic"
+});
